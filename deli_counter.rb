1634b074502495f7dea9cb494209207bcbec378a
kevin xirum <kevinmenjivar66@gmail.com>
2020-05-16 20:20:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(deli)
+  if deli.length== 0
+    puts "The line is currently empty."
+  else 
+    current_line = "The line is currently:"
+    deli.each_with_index do |customer, index|
+      current_line << " #{index + 1}. #{customer}"
+    end
+    puts current_line
+ end
+end
